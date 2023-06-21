f8f0a311f7830906b7ce74cc35e0e44963af9dbf
Lulifernandez <127899585+Lulifernandez@users.noreply.github.com>
2023-06-20 23:27:38 -0300

    toggle btn / cards

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-// function mostrarMensaje() {
-//     alert("funciona Javascript")
-    
-// }
+//<----------------Toggle Button---------------->
+// Obtener todos los botones de "More Info"
+const toggleButtons = document.querySelectorAll('.toggle-button');
+
+// Agregar un evento de escucha a cada botón
+toggleButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    // Obtener el elemento de detalles adicionales correspondiente
+    const moreInfo = button.nextElementSibling;
+
+    // Alternar la visibilidad de los detalles adicionales
+    if (moreInfo.style.display === 'none') {
+      moreInfo.style.display = 'block';
+    } else {
+      moreInfo.style.display = 'none';
+    }
+  });
+});
 
 
 
